1043c46a206b93911520a4e5a9b3847441f39f97
ptstory <perry.t.story@gmail.com>
2019-04-07 20:57:30 -0400

    added Betting contract, can start bet and get running bool from front end

---
+++ src/views/Betting.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="betting">
+    <Bet />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Bet from "@/components/Bet.vue";
+
+export default {
+  name: "betting",
+  components: {
+    Bet
+  }
+};
+</script>
