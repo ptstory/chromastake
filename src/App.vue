3774b427f39ffa4a6b675d846a3fa92be9d2869e
earamburo <31488734+earamburo@users.noreply.github.com>
2019-04-14 19:34:24 -0400

    header fully functional

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/betting">Betting</router-link>
+      <b-navbar toggleable="lg" type="dark" class="nav-background">
+        <img src ="./assets/chromostake_logo.png" height="25" width="30" class="project_logo">
+        <b-navbar-brand href="">Chromastake</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav  >
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/about">About</b-nav-item>
+            <b-nav-item to="/betting">Betting</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+
     </div>
     <router-view />
   </div>
@@ -17,14 +29,38 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.nav-background{
+  background-color:#333333;
 }
+.project_logo{
+  margin-bottom: 7px;
+}
+.navbar-brand{
+  color:rgb(78, 184, 131)!important;
+  font-weight: bold;
+}
+.nav-link{
+color:white!important;
+font-weight: bold;
+}
+.navbar-dark .navbar-nav .nav-link:hover {
+  color:rgb(78, 184, 131)!important;
+  font-weight:bold;
+}
+.router-link-exact-active{
+  color:rgb(78, 184, 131)!important;
+}
+
+
+
+// #nav {
+//   padding: 30px;
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
